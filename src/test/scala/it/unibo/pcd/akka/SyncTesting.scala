b97c0e94838680640e0ce0a4e55e174ef7c6ea2d
Roberto Casadei <robyonrails@gmail.com>
2021-05-09 13:52:55 +0200

    Example of sync testing

---
+++ src/test/scala/it/unibo/pcd/akka/SyncTesting.scala
@@ -0,0 +1,74 @@
+package it.unibo.pcd.akka
+
+import akka.actor.testkit.typed.Effect
+import akka.actor.testkit.typed.Effect.SpawnedAnonymous
+import akka.actor.testkit.typed.scaladsl.{BehaviorTestKit, TestInbox}
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import org.scalatest.funsuite.AnyFunSuite
+
+object ActorUnderTest {
+  sealed trait Command
+  object SpawnChild extends Command
+  object SpawnChildAnonymous extends Command
+  object SayHelloToChild extends Command
+  object SayHelloToAnonymousChild extends Command
+  case class Ping(replyTo: ActorRef[Pong.type]) extends Command
+
+  sealed trait Response
+  object Pong extends Response
+
+  val child = Behaviors.receiveMessage[Any] { _ => Behaviors.same }
+
+  def apply(): Behavior[Command] = Behaviors.receive {
+    case (ctx, SpawnChild) =>
+      ctx.spawn(child, "child")
+      Behaviors.same
+    case (ctx, SpawnChildAnonymous) =>
+      ctx.spawnAnonymous(child)
+      Behaviors.same
+    case (_, Ping(replyTo)) =>
+      replyTo ! Pong
+      Behaviors.same
+    case (ctx, SayHelloToChild) =>
+      ctx.child("child").get.unsafeUpcast[String] ! "hello"
+      Behaviors.same
+    case (ctx, SayHelloToAnonymousChild) =>
+      ctx.spawnAnonymous(child) ! "hello"
+      Behaviors.same
+  }
+}
+
+class SyncTesting extends AnyFunSuite {
+  import ActorUnderTest._
+
+  test("Child actor spawning") {
+    val testKit = BehaviorTestKit(ActorUnderTest())
+    testKit.run(SpawnChild)
+    testKit.expectEffect(Effect.Spawned(child, "child"))
+    testKit.run(SpawnChildAnonymous)
+    testKit.expectEffect(Effect.SpawnedAnonymous(child))
+  }
+
+  test("Sending messages to another actor") {
+    val testKit = BehaviorTestKit(ActorUnderTest())
+    val inbox = TestInbox[Pong.type]()
+    testKit.run(Ping(inbox.ref))
+    inbox.expectMessage(Pong)
+  }
+
+  test("Sending messages to child") {
+    val testKit = BehaviorTestKit(ActorUnderTest())
+    testKit.run(SpawnChild)
+    val childInbox = testKit.childInbox[String]("child")
+    testKit.run(SayHelloToChild)
+    childInbox.expectMessage("hello")
+
+    // For anonymous children the actor names are generated in a deterministic way:
+    testKit.run(SayHelloToAnonymousChild)
+    testKit.expectEffectType[Effect.Spawned[String]] // must consume old effect
+    val child: Effect.SpawnedAnonymous[String] = testKit.expectEffectType[Effect.SpawnedAnonymous[String]]
+    val achildInbox = testKit.childInbox(child.ref)
+    achildInbox.expectMessage("hello")
+  }
+}
